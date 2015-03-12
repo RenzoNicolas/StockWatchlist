cf0dd40b03b16c4ef4f733d9144539396fff365d
RenzoNicolas <renzo_nicolas@outlook.com>
2015-03-11 21:25:07 -0400

    modefied addrow file

--- js/Add.js
+++ js/Add.js
@@ -6,7 +6,7 @@ function AddNewRow() {
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
     var cell4 = row.insertCell(3);
-    cell1.innerHTML = '<input size= "4" id= "StockName">';
+    cell1.innerHTML = '<input size= "4" maxlength= "4" id= "StockName">';
     cell2.innerHTML = "[Price]";
     cell3.innerHTML = "[Change]";
     cell4.innerHTML = '<p class= "DeleteRow" onclick= "deleteRow(this)">(x)</p>';
