6fb1649d99430f4ac02ef2f1204bafce9848d246
RenzoNicolas <renzo_nicolas@outlook.com>
2015-03-11 21:15:47 -0400

    Framework off stock watchlist table with add/remove row functionality

---
+++ js/Add.js
@@ -0,0 +1,13 @@
+//'use strict';
+function AddNewRow() {
+    var table = document.getElementById("table1");
+    var row = table.insertRow(2);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    cell1.innerHTML = '<input size= "4" id= "StockName">';
+    cell2.innerHTML = "[Price]";
+    cell3.innerHTML = "[Change]";
+    cell4.innerHTML = '<p class= "DeleteRow" onclick= "deleteRow(this)">(x)</p>';
+}
