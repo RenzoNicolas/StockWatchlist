9cf354f5420a706d961f4bc8d5e12e09e8a75d48
RenzoNicolas <renzo_nicolas@outlook.com>
2015-03-14 19:51:12 -0400

    fixed autocomplete

---
+++ js/stockTicker.js
@@ -0,0 +1,3 @@
+function stockTicker() {
+   <p id="enterStock"><input id="autocomplete" type="text" name="currency" maxlength= "4" size= "4"/><span id="stock">Stock Name</span></input></p><script> document.getElementById("autocomplete").style.display = "none"; </script>
+}
